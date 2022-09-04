228b346d2a81b1950037c4d8ac1fa50c0bfcb36f
Odai Alali <odai.alali@neuland-bfi.de>
2022-09-04 19:47:24 +0200

    ci: upload cypress odiff artifacts

--- examples/cypress10/cypress/e2e/failing-visual-regression.cy.js
+++ examples/cypress10/cypress/e2e/failing-visual-regression.cy.js
@@ -1,42 +1,33 @@
 /// <reference types="cypress-odiff" />
 
-
-
 describe("visual regression", () => {
-
-  Cypress.on('fail', (err, runnable) => {
+  Cypress.on("fail", (err, runnable) => {
     const expectedErrors = [
       "Screenshots does not match. 1251 pixels difference (0.0715675057208%)",
-      "Expected image is missing"
-    ]
+      "Expected image is missing",
+    ];
     if (!expectedErrors.includes(err.message)) {
-      throw err
+      throw err;
     }
-  })
+  });
   it("should fail when expected screenshot is missing", () => {
-
-    cy.visit('https://example.cypress.io')
-    cy.wait(1000)
+    cy.visit("https://example.cypress.io");
     cy.compareScreenshot({
       screenshotOptions: {
-        capture: "viewport"
+        capture: "viewport",
       },
-      pluginOptions:{
-        failOnExpectedMissing: true
-      }
-    })
-  })
+      pluginOptions: {
+        failOnExpectedMissing: true,
+      },
+    });
+  });
 
   it("should fail when diff is detected", () => {
-
-    cy.visit('https://example.cypress.io')
-    cy.wait(1000)
+    cy.visit("https://example.cypress.io");
     cy.compareScreenshot({
       screenshotOptions: {
-        capture: "viewport"
+        capture: "viewport",
       },
-    })
-  })
-
-
+    });
+  });
 });
--- examples/cypress10/cypress/e2e/visual-regression.cy.js
+++ examples/cypress10/cypress/e2e/visual-regression.cy.js
@@ -1,15 +1,12 @@
 /// <reference types="cypress-odiff" />
 
-
-
 describe("visual regression", () => {
   it("should compare screenshots", () => {
-    cy.visit('https://example.cypress.io')
-    cy.wait(1000)
+    cy.visit("https://example.cypress.io");
     cy.compareScreenshot({
       screenshotOptions: {
-        capture: "viewport"
-      }
-    })
+        capture: "viewport",
+      },
+    });
   });
 });
